66842856fd84db6c7492df3236fc6833e24f34de
Xinyu Fang <f813779259@gmail.com>
2025-10-31 23:04:14 -0400

    Initial Commit

---
+++ Buzz/BuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzApp.swift
+//  Buzz
+//
+//  Created by Xinyu Fang on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buzz
+//
+//  Created by Xinyu Fang on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
